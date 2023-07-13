1938add5152f1b0930cf6de0c54e1dd3413c2894
Lane Bretschneider <lanebret85@gmail.com>
2023-07-13 09:25:01 -0700

    feat: add long_term_players and short_term_players methods

--- lib/team.rb
+++ lib/team.rb
@@ -14,4 +14,24 @@ class Team
   def add_player(player)
     @roster << player
   end
+
+  def long_term_players
+    players = []
+    @roster.each do |player|
+      if player.contract_length > 24
+        players << player
+      end
+    end
+    players
+  end
+
+  def short_term_players
+    players = []
+    @roster.each do |player|
+      if player.contract_length <= 24
+        players << player
+      end
+    end
+    players
+  end
 end
