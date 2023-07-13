883d5f96010eff7e341dc3f4af546aae9b992f9c
Lane Bretschneider <lanebret85@gmail.com>
2023-07-13 08:51:15 -0700

    test: add test for exists

---
+++ spec/team_spec.rb
@@ -0,0 +1,10 @@
+require './lib/player'
+require './lib/team'
+
+RSpec.describe Team do
+  it "exists" do
+    team = Team.new("Dodgers", "Los Angeles")
+
+    expect(team).to be_a(Team)
+  end
+end
