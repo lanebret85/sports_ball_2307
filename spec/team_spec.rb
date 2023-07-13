9db2723f6fd4ca1b7f9e32dd216241bcec8b6e01
Lane Bretschneider <lanebret85@gmail.com>
2023-07-13 08:57:35 -0700

    test: add test for add_player method

--- spec/team_spec.rb
+++ spec/team_spec.rb
@@ -13,4 +13,21 @@ RSpec.describe Team do
 
     expect(team.roster).to eq([])
   end
+
+  it "can count players" do
+    team = Team.new("Dodgers", "Los Angeles")
+
+    expect(team.player_count).to eq(0)
+  end
+
+  it "can add players" do
+    team = Team.new("Dodgers", "Los Angeles")
+    player_1 = Player.new("Michael Palledorous" , 1000000, 36)
+    player_2 = Player.new("Kenny DeNunez", 500000, 24)
+    team.add_player(player_1)
+    team.add_player(player_2)
+
+    expect(team.roster).to eq([player_1, player_2])
+    expect(team.player_count).to eq(2)
+  end
 end
