2bef73562fd96690a000114465494a6a034f8eeb
Lane Bretschneider <lanebret85@gmail.com>
2023-07-13 09:13:52 -0700

    test: add test for short_term_players method

--- spec/team_spec.rb
+++ spec/team_spec.rb
@@ -44,4 +44,18 @@ RSpec.describe Team do
     
     expect(team.long_term_players).to eq([player_1, player_3])
   end
+
+  it "can return short term players" do
+    team = Team.new("Dodgers", "Los Angeles")
+    player_1 = Player.new("Michael Palledorous" , 1000000, 36)
+    player_2 = Player.new("Kenny DeNunez", 500000, 24)
+    player_3 = Player.new("Alan McClennan", 750000, 48)
+    player_4 = Player.new("Hamilton Porter", 100000, 12)
+    team.add_player(player_1)
+    team.add_player(player_2)
+    team.add_player(player_3)
+    team.add_player(player_4)
+
+    expect(team.short_term_players).to eq([player_4, player_2])
+  end
 end
