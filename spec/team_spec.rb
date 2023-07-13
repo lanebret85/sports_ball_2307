229a0ebf87ca92a9bdb43076c24a2e590f91ce44
Lane Bretschneider <lanebret85@gmail.com>
2023-07-13 09:15:12 -0700

    test: add test for total_value method

--- spec/team_spec.rb
+++ spec/team_spec.rb
@@ -58,4 +58,18 @@ RSpec.describe Team do
 
     expect(team.short_term_players).to eq([player_4, player_2])
   end
+
+  it "can return total value of team" do
+    team = Team.new("Dodgers", "Los Angeles")
+    player_1 = Player.new("Michael Palledorous" , 1000000, 36)
+    player_2 = Player.new("Kenny DeNunez", 500000, 24)
+    player_3 = Player.new("Alan McClennan", 750000, 48)
+    player_4 = Player.new("Hamilton Porter", 100000, 12)
+    team.add_player(player_1)
+    team.add_player(player_2)
+    team.add_player(player_3)
+    team.add_player(player_4)
+
+    expect(team.total_value).to eq(85200000)
+  end
 end
